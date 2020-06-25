70eee6da5de9d9c8c9888c23768fa2ca39c53db3
intBethune <863733676@qq.com>
2020-06-25 18:51:28 +0800

    2nd（commit message为“1st”的代码为第一个成品。此文件为第二个成品，其区别在于重要项的设计和呈现，由于两个代码都能清楚表达重要项，于是我把它分别上传，以供检查。）

---
+++ web提交版2nd/scripts.js
@@ -0,0 +1,168 @@
+
+let tasks = [];
+
+
+
+
+function renderEditor() {
+  let inputEl = document.querySelector("#default--todo--panel .todo--editor > input");
+ 
+
+
+  let addTask = () => {
+    if (inputEl.value.length == 0) {
+      return;
+    }
+    let newTask = {
+      title: inputEl.value,
+      done: false,
+      im: false
+    };
+
+    inputEl.value = "";
+
+    tasks.push(newTask);
+
+
+    console.log("tasks:", tasks);
+
+    renderTaskItems();
+  }
+
+  inputEl.onkeypress = (e) => {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  }
+
+  let addEl = document.querySelector("#default--todo--panel .todo--editor > button");
+
+  addEl.onclick = (e) => {
+    addTask();
+  };
+}
+
+function renderTaskItems() {
+  let itemsEl = document.querySelector("#default--todo--panel .todo--items");
+
+  itemsEl.querySelectorAll("div").forEach((node) => node.remove());
+
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    let itemEl = document.createElement("div");
+    itemEl.className = "task";
+
+
+    let doneEL = document.createElement("input");
+    doneEL.type = "checkbox";
+    if (task.done) {
+      itemEl.classList.add("done");
+    } else {
+      itemEl.classList.remove("done");
+    }
+
+    doneEL.checked = task.done;
+    doneEL.onchange = (e) => {
+      task.done = e.target.checked;
+      if (task.done) {
+        itemEl.classList.add("done");
+      } else {
+        itemEl.classList.remove("done");
+      }
+    }
+    itemEl.append(doneEL);
+
+    let titleEL = document.createElement("label");
+    titleEL.innerText = task.title;
+    itemEl.append(titleEL);
+
+    let ctrbarEL = renderTaskCtrlBar(task, i, itemEl);
+
+    itemEl.append(ctrbarEL);
+
+    itemsEl.append(itemEl);
+  }
+}
+
+function renderTaskCtrlBar(task, taskIdx, itemEl) {
+  let ctrbarEL = document.createElement("div");
+  ctrbarEL.className = "ctrlbar";
+
+  let importantEL = document.createElement("button");
+  if (tasks[taskIdx].im) {
+    importantEL.innerText = "⭐";
+  } else {
+    importantEL.innerText = "☆";
+  }
+
+
+  importantEL.onclick = () => {
+    console.log("add click");
+    
+    if (tasks[taskIdx].im) {
+      tasks[taskIdx].im = false;
+      renderTaskItems();
+    } else {
+      tasks[taskIdx].im = true;
+      renderTaskItems();
+    };
+    
+    renderTaskItems();
+  } 
+  ctrbarEL.append(importantEL);
+
+
+
+
+  let upEL = document.createElement("button");
+  if (taskIdx === 0) {
+    upEL.disabled = true;
+  }
+  upEL.innerText = "🠕";
+  upEL.onclick = () => {
+    var tmp = tasks[taskIdx];
+    tasks[taskIdx] = tasks[taskIdx - 1]
+    tasks[taskIdx - 1] = tmp;
+    console.log();
+    renderEditor();
+    renderTaskItems();
+  };
+  ctrbarEL.append(upEL);
+
+  
+  let downEL = document.createElement("button");
+  downEL.innerText = "🠗";
+  if (taskIdx === tasks.length - 1) {
+    downEL.disabled = true;
+  }
+  downEL.onclick = () => {
+    var tmp = tasks[taskIdx];
+    tasks[taskIdx] = tasks[taskIdx + 1]
+    tasks[taskIdx + 1] = tmp;
+    console.log();
+    renderEditor();
+    renderTaskItems();
+  };
+
+  ctrbarEL.append(downEL);
+
+
+  
+  let cancelEL = document.createElement("button");
+  cancelEL.innerText = "X";
+  cancelEL.onclick = () => {
+    tasks.splice(taskIdx, 1);
+    renderTaskItems();
+  };
+  ctrbarEL.append(cancelEL);
+
+
+ 
+
+ 
+
+  return ctrbarEL;
+}
+
+renderEditor();
+renderTaskItems();
